f8dfecf4cea68dc1a5aef0586b89f37c3be4bcd1
Vincent Swarte <vincentswarte@gmail.com>
2023-02-13 11:02:18 +0100

    Rename StandardScanner to SimpleScanner

--- shared/byte_scanner/src/scanner.rs
+++ shared/byte_scanner/src/scanner.rs
@@ -5,9 +5,9 @@ trait Scanner {
 }
 
 #[derive(Default)]
-struct StandardScanner;
+struct SimpleScanner;
 
-impl Scanner for StandardScanner {
+impl Scanner for SimpleScanner {
     fn scan(&self, scannable: &[u8], pattern: &MemoryPattern) -> Option<usize> {
         let mut position_in_pattern = 0;
 
@@ -37,13 +37,13 @@ impl Scanner for StandardScanner {
 mod tests {
     use crate::scanner::Scanner;
     use crate::pattern::MemoryPattern;
-    use crate::scanner::StandardScanner;
+    use crate::scanner::SimpleScanner;
 
     #[test]
     fn standard_scanner_behaves_with_empty_slice() {
         let pattern = MemoryPattern::from_ida_pattern("AA AA AA AA AA").unwrap();
         let slice = [];
-        let result = StandardScanner::default().scan(&slice, &pattern);
+        let result = SimpleScanner::default().scan(&slice, &pattern);
 
         assert_eq!(result, None);
     }
@@ -52,7 +52,7 @@ mod tests {
     fn standard_scanner_behaves_with_too_long_of_an_aob() {
         let pattern = MemoryPattern::from_ida_pattern("AA AA AA AA AA").unwrap();
         let slice = [0x00, 0x00, 0x00, 0x00];
-        let result = StandardScanner::default().scan(&slice, &pattern);
+        let result = SimpleScanner::default().scan(&slice, &pattern);
 
         assert_eq!(result, None);
     }
@@ -61,7 +61,7 @@ mod tests {
     fn standard_scanner_finds_the_pattern_1() {
         let pattern = MemoryPattern::from_ida_pattern("75 84 4A EF 23 24 CA 35").unwrap();
         let randomness = include_bytes!("../../../tests/random.bin");
-        let result = StandardScanner::default().scan(randomness, &pattern).unwrap();
+        let result = SimpleScanner::default().scan(randomness, &pattern).unwrap();
 
         assert_eq!(result, 1309924);
     }
@@ -70,7 +70,7 @@ mod tests {
     fn standard_scanner_finds_the_pattern_2() {
         let pattern = MemoryPattern::from_ida_pattern("B7 ?? CF D8 ?? 0A ?? 27").unwrap();
         let randomness = include_bytes!("../../../tests/random.bin");
-        let result = StandardScanner::default().scan(randomness, &pattern).unwrap();
+        let result = SimpleScanner::default().scan(randomness, &pattern).unwrap();
 
         assert_eq!(result, 867776);
     }
@@ -79,7 +79,7 @@ mod tests {
     fn standard_scanner_doesnt_find_the_pattern() {
         let pattern = MemoryPattern::from_ida_pattern("FF FF FF FF FF FF FF FF").unwrap();
         let randomness = include_bytes!("../../../tests/random.bin");
-        let result = StandardScanner::default().scan(randomness, &pattern);
+        let result = SimpleScanner::default().scan(randomness, &pattern);
 
         assert_eq!(result, None);
     }
