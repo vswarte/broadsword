e9ec4f3bd0c6338ceefee0071a42abea41c68e0c
Vincent Swarte <vincentswarte@gmail.com>
2023-04-12 18:48:23 +0200

    actually expose scanner trait

--- crates/broadsword_aob/src/scanner/mod.rs
+++ crates/broadsword_aob/src/scanner/mod.rs
@@ -3,9 +3,7 @@ use crate::pattern::Pattern;
 pub mod simple;
 pub mod threaded;
 
-// Because of the shortcuts I made further down the line to prevent copying this scannable has to
-// be static. This is fine for my needs as I'll be dealing with memory that isn't managed by rust.
-trait Scanner {
+pub trait Scanner {
     fn scan(&self, scannable: &'static [u8], pattern: &Pattern) -> Option<ScanResult>;
 }
 
