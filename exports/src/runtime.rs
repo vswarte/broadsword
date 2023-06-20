42aef4d93425338e6299536ce5acaffee7634f80
Vincent Swarte <vincentswarte@gmail.com>
2023-06-20 22:26:53 +0200

    Expose runtime::get_module_handle

--- exports/src/runtime.rs
+++ exports/src/runtime.rs
@@ -2,6 +2,7 @@ pub use broadsword_microsoft_runtime::memory::remove_pageguard;
 pub use broadsword_microsoft_runtime::memory::set_pageguard;
 
 pub use broadsword_microsoft_runtime::module::Module;
+pub use broadsword_microsoft_runtime::module::get_module_handle;
 pub use broadsword_microsoft_runtime::module::get_module_pointer_belongs_to;
 
 pub use broadsword_microsoft_runtime::pointer::is_valid_pointer;
